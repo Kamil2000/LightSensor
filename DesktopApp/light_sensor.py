76c693466cd800d1c83cb65a30b464aaf9fce8d6
Kamil Adamski <kamil@debianlaptop.debianlaptop>
2022-05-21 16:21:46 +0200

    Small fix 1

--- DesktopApp/light_sensor.py
+++ DesktopApp/light_sensor.py
@@ -105,13 +105,14 @@ def open_serial(device):
         serial_port.baudrate=9600
         serial_port.timeout = 0.3
         serial_port.open()
+        serial_port.read(1000)
+        serial_port.timeout = 1
         # Workaround for a chip FTDI. For a reason unknown to me
         # that chip works differently than CH* chips and records
         # data that had been sent before serial was opened.
         # Moreover it can actually give the same lines twice
         # for a differenct instances of serial (if someone opens and closes
         # it in a simmilar way as I do it)
-        serial_port.read(1000)
         serial_wrapper = sensor_utils.StreamWrapper(serial_port, 100)
         return (serial_port, serial_wrapper)
     except:
@@ -426,6 +427,7 @@ class ApplicationController:
             return
         (serial, reader) = opened_serial
         try:
+            
             self.device_name = device_txt
             serial.read_until(':')
             if proc.nop(reader) != proc.CMD_SUCCESS:
